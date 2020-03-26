ffd23e2b66e597999ef262b895a4e6b5c94a9bdd
jsshap <jsshap@gmail.com>
2020-03-26 16:53:41 -0400

    Update PullImageFromImage.java

--- PullImageFromImage.java
+++ PullImageFromImage.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 //original example from
 // w w w  . jav  a 2s  . com
 //example change
+//another change
 import javax.imageio.ImageIO;
 
 public class PullImageFromImage {
