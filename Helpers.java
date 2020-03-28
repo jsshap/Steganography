bf39e5a78a130819ffd2c69e1d54be835cbed1ec
jsshap <jsshap@gmail.com>
2020-03-28 10:47:27 -0400

    wrote 3 helpers

--- Helpers.java
+++ Helpers.java
@@ -52,11 +52,14 @@ public class Helpers{
     }
     public static String convertBytesToString(LinkedList<Integer> bytes){
         //TODO: test this
-
+        LinkedList<Integer> localBytes = new LinkedList<Integer>();
+        for (Integer i : bytes){localBytes.add(i);}//Copies bytes into a local variable
+        //This method should not modify the List called bytes, rather copy it and
+        //modify the local version
         String toReturn = "";
         //insert code here to throwaway header/garbage bytes
-        while (!bytes.isEmpty()){//change this line if you want to specify length
-            toReturn += (char) bytes.removeFirst().intValue();
+        for (int i=0; i<100; i++){//change this line if you want to specify length
+            toReturn += (char) localBytes.removeFirst().intValue();
         }
 
         return toReturn;
@@ -80,5 +83,42 @@ public class Helpers{
         return bits;
     
     }
+    public static LinkedList<Integer> pullLSBsOfSpecificColors(BufferedImage image, int[] colors){
+        //TODO test this method
+        //Pull LSBs of one color channel. R=0, G=1, B=2. Input array must be in increasing order
+        int width = image.getWidth();
+        int height = image.getHeight();
+       // System.out.println("Height: " + height + " Width: " + width);
+        
+        LinkedList<Integer> bits = new LinkedList<Integer>();
+        WritableRaster raster = image.getRaster();
+        for (int r = 0; r < height; r++) {
+            for (int c = 0; c < width; c++) {
+                int[] pixels = raster.getPixel(c, r, (int[]) null);
+                for (int i : colors){
+                    bits.add(pixels[i]&1); 
+                }
+            }
+        }
+        return bits;
+    }
+    public static void checkForHeader(LinkedList<Integer> bytes){
+        //TODO test. should probably use the hidden image where we know the header
+        //Print out the first three ints in big and little endian. as 32 bit ints
+        int[] firstBytes = new int[12];
+        for (int i =0; i < 12; i++){
+            firstBytes[i]=bytes.get(i);
+        }
+        int intOneBigEndian= (firstBytes[0]<<24) + (firstBytes[1]<<16)+(firstBytes[2]<<8)+(firstBytes[3]);
+        int intTwoBigEndian = (firstBytes[4]<<24) + (firstBytes[5]<<16)+(firstBytes[6]<<8)+(firstBytes[7]);
+        int intThreeBigEndian = (firstBytes[8]<<24) + (firstBytes[9]<<16)+(firstBytes[10]<<8)+(firstBytes[11]);
 
+
+        int intOneLittleEndian = (firstBytes[3]<<24) + (firstBytes[2]<<16)+(firstBytes[1]<<8)+(firstBytes[0]);
+        int intTwoLittleEndian = (firstBytes[7]<<24) + (firstBytes[6]<<16)+(firstBytes[5]<<8)+(firstBytes[4]);
+        int intThreeLittleEndian= (firstBytes[11]<<24) + (firstBytes[10]<<16)+(firstBytes[9]<<8)+(firstBytes[8]);
+
+        System.out.println("Big Endian: " + intOneBigEndian + ", " + intTwoBigEndian+ ", " + intThreeBigEndian);
+        System.out.println("Little Endian: " + intOneLittleEndian + ", " + intTwoLittleEndian+ ", " + intThreeLittleEndian);
+    }
 }
